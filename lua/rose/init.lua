99af71d572d23f72b03a844401e0674823f227b3
PhaedrusFlow <map@qompass.ai>
2024-11-27 22:17:56 -0800

    update

--- lua/rose/init.lua
+++ lua/rose/init.lua
@@ -4,6 +4,22 @@ M.did_setup = false
 
 local default_opts = {
   providers = { "ollama" },
+  ollama = {
+    endpoint = "http://localhost:11434/api/chat",
+    topic_prompt = [[
+      Summarize the chat above and only provide a short headline of 2 to 3
+      words without any opening phrase like "Sure, here is the summary",
+      "Sure! Here's a shortheadline summarizing the chat" or anything similar.
+    ]],
+    topic = {
+      model = "smollm2:135m",
+      params = { max_tokens = 32 },
+    },
+    params = {
+      chat = { temperature = 1.5, top_p = 1, num_ctx = 8192, min_p = 0.05 },
+      command = { temperature = 1.5, top_p = 1, num_ctx = 8192, min_p = 0.05 },
+    },
+  },
 }
 
 ---@param opts? table
