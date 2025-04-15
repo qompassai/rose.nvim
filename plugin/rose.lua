64b6252cc3953a3140553d3c6ea7be83b352b741
phaedrusflow <map@qompass.ai>
2025-04-15 12:20:47 -0700

    update

--- plugin/rose.lua
+++ plugin/rose.lua
@@ -1,3 +1,6 @@
+-- rose.nvim/plugin/rose.lua
+---@type LazyPluginSpec
+
 for _, name in ipairs({ "curl", "grep", "rg", "ln" }) do
   if vim.fn.executable(name) == 0 then
     return vim.notify(name .. " is not installed, run :checkhealth rose", vim.log.levels.ERROR)
@@ -15,3 +18,19 @@ timer:start(
     end
   end)
 )
+
+return {
+  "qompassai/rose.nvim",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "rcarriga/nvim-notify",
+    "folke/noice.nvim",
+    "folke/edgy.nvim",
+  },
+  config = function()
+    require("rose").setup()
+  end,
+}
+
