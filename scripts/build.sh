5de64be145ab53d68d7e19bcb2197c8d6d5c38d6
phaedrusflow <map@qompass.ai>
2025-07-19 12:00:19 -0700

    Release metadata updates for 2025-07-19

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,41 +1,43 @@
 #!/usr/bin/env bash
-# /qompassai/rose.nvim/build.sh
-# -----------------------------
+# /qompassai/rose.nvim/scripts/build.sh
+# Qompass AI Rose.nvim Build Script
 # Copyright (C) 2025 Qompass AI, All rights reserved
+#####################################################
+
 set -e
 REPO_OWNER="qompassai"
 REPO_NAME="rose.nvim"
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 TARGET_DIR="${SCRIPT_DIR}/build"
 case "$(uname -s)" in
-    Linux*)
-        PLATFORM="linux"
-        ;;
-    Darwin*)
-        PLATFORM="darwin"
-        ;;
-    CYGWIN* | MINGW* | MSYS*)
-        PLATFORM="windows"
-        ;;
-    *)
-        echo "Unsupported platform"
-        exit 1
-        ;;
+Linux*)
+    PLATFORM="linux"
+    ;;
+Darwin*)
+    PLATFORM="darwin"
+    ;;
+CYGWIN* | MINGW* | MSYS*)
+    PLATFORM="windows"
+    ;;
+*)
+    echo "Unsupported platform"
+    exit 1
+    ;;
 esac
 case "$(uname -m)" in
-    x86_64)
-        ARCH="x86_64"
-        ;;
-    aarch64)
-        ARCH="aarch64"
-        ;;
-    arm64)
-        ARCH="aarch64"
-        ;;
-    *)
-        echo "Unsupported architecture"
-        exit 1
-        ;;
+x86_64)
+    ARCH="x86_64"
+    ;;
+aarch64)
+    ARCH="aarch64"
+    ;;
+arm64)
+    ARCH="aarch64"
+    ;;
+*)
+    echo "Unsupported architecture"
+    exit 1
+    ;;
 esac
 LUA_VERSION="${LUA_VERSION:-luajit}"
 ARTIFACT_NAME_PATTERN="rose_lib-$PLATFORM-$ARCH-$LUA_VERSION"
